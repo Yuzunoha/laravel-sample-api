8fd7badf42cf12cec2eefcffab71e280725fa42f
yuzunoha <nurugamer2@gmail.com>
2020-01-02 02:38:04 +0900

    新規作成と詳細表示を実装した

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -27,7 +27,7 @@ class ArticlesController extends Controller
      */
     public function create()
     {
-        //
+        return view('articles.create');
     }
 
     /**
@@ -38,7 +38,15 @@ class ArticlesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // モデルからインスタンスを生成
+        $article = new Article;
+        // $requestにformからのデータが格納されているので、以下のようにそれぞれ代入する
+        $article->title = $request->title;
+        $article->body = $request->body;
+        // 保存
+        $article->save();
+        // 保存後 一覧ページへリダイレクト
+        return redirect('/articles');
     }
 
     /**
@@ -49,7 +57,10 @@ class ArticlesController extends Controller
      */
     public function show($id)
     {
-        //
+        // 引数で受け取った$idを元にfindでレコードを取得
+        $article = Article::find($id);
+        // viewにデータを渡す
+        return view('articles.show', ['article' => $article]);
     }
 
     /**
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -2,17 +2,12 @@
 @extends('layouts.application')
 
 {{-- @yield('title')にテンプレートごとの値を代入 --}}
-@section('title', '記事一覧')
+@section('title', '記事詳細')
 
 {{-- application.blade.phpの@yield('content')に以下のレイアウトを代入 --}}
 @section('content')
-  <div>
-    <a href="/articles/create">新規作成</a>
-  </div>
-  @foreach ($articles as $article)
-    <h4>{{$article->title}}</h4>
-    <p>{{$article->body}}</p>
-    <a href="/articles/{{$article->id}}">詳細を表示</a>
-    <hr>
-  @endforeach
+  <h1>{{$article->title}}</h1>
+  <p>{{$article->body}}</p>
+  <br><br>
+  <a href="/articles">一覧に戻る</a>
 @endsection
